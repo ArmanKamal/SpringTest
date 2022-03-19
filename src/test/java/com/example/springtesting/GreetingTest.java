3bc4c61d8cbe30dd3d91b623ef46abfd2aa9a7c9
ArmanKamal <armankamal2421@gmail.com>
2022-03-19 02:29:34 -0500

    Junit lifecycle

--- src/test/java/com/example/springtesting/GreetingTest.java
+++ src/test/java/com/example/springtesting/GreetingTest.java
@@ -1,7 +1,6 @@
 package com.example.springtesting;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,19 +8,39 @@ class GreetingTest {
 
     private Greeting greeting;
 
+    @BeforeAll
+    static void beforeClass(){
+        System.out.println("Called once before all test execution");
+    }
+
     @BeforeEach
     public void setUp(){
+        System.out.println("Called before each test  ");
         greeting = new Greeting();
     }
 
 
     @Test
     void helloWorld() {
-            System.out.println(greeting.helloWorld());
+        System.out.print("First Test ");
+        System.out.println(greeting.helloWorld());
     }
 
     @Test
     void testHelloWorld() {
+        System.out.print("Second Test ");
         System.out.println(greeting.helloWorld("John Cena"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Called after each test");
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Called last after all test are done !Once");
+
     }
 }
